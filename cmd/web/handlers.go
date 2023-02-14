fe3ad2ec35879ccdcd7cbf7295624e45057fa63e
Philip-21 <philipuzomaobiora@gmail.com>
2023-02-14 18:51:14 +0100

    writing handlers

---
+++ cmd/web/handlers.go
@@ -0,0 +1,21 @@
+package main
+
+import "net/http"
+
+func (app *Config) homePage(w http.ResponseWriter, r *http.Request) {
+	app.render(w, r, "home.page.gohtml", nil)
+}
+
+func (app *Config) Loginpage(w http.ResponseWriter, r *http.Request) {
+	app.render(w, r, "login.page.gohtml", nil)
+}
+
+func (app *Config) PostLoginpage(w http.ResponseWriter, r *http.Request) {}
+
+func (app *Config) Logout(w http.ResponseWriter, r *http.Request) {}
+
+func (app *Config) RegisterPage(w http.ResponseWriter, r *http.Request) {
+	app.render(w, r, "register.page.gohtml", nil)
+}
+
+func (app *Config) PostRegisterPage(w http.ResponseWriter, r *http.Request) {}
