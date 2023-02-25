d0741eea9fd52ad37e46345ebd7c94387065f0be
Philip-21 <philipuzomaobiora@gmail.com>
2023-02-25 09:19:50 +0100

    adding errorlistener functions for go routines removing the send email helper

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -49,9 +49,23 @@ func (app *Config) Shutdown() {
 	app.Wait.Wait()
 
 	app.Mailer.DoneChan <- true
+	app.ErrorChanDone <- true
 
 	app.InfoLog.Println("closing channels and shutting down application..")
 	close(app.Mailer.MailerChan)
 	close(app.Mailer.ErrorChan)
 	close(app.Mailer.DoneChan)
+	close(app.ErrorChan)
+	close(app.ErrorChanDone)
+}
+
+func (app *Config) ListenForErrors() {
+	for {
+		select {
+		case err := <-app.ErrorChan:
+			app.ErrorLog.Println(err)
+		case <-app.ErrorChanDone:
+			return
+		}
+	}
 }
