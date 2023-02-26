2087a81ff79adc7bc74803676fb0734769a9d7ae
Philip-21 <philipuzomaobiora@gmail.com>
2023-02-26 08:08:36 +0100

    subscription plan handler,invoice and manual handlers created, writing log messages

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
+	"strconv"
 	"subscription-service/database"
+	"time"
+
+	"github.com/phpdave11/gofpdf"                //This library allows us to create a pdf
+	"github.com/phpdave11/gofpdf/contrib/gofpdi" //this library opens an existing pdf and uses it as a template
 )
 
 func (app *Config) homePage(w http.ResponseWriter, r *http.Request) {
@@ -91,12 +98,16 @@ func (app *Config) PostRegisterPage(w http.ResponseWriter, r *http.Request) {
 	_, err = u.Insert(u)
 	if err != nil {
 		app.Session.Put(r.Context(), "error", "Unable to create a user")
+		app.ErrorLog.Println("Unable to Create user")
 		http.Redirect(w, r, "/register", http.StatusSeeOther)
 	}
+	app.InfoLog.Println("User Registered")
 	// send an activation email
 	url := fmt.Sprintf("http://localhost/activate?email=%s", u.Email)
 	signedUrl := GenerateTokenFromString(url) //prevents url from being tampered
 	app.InfoLog.Println(signedUrl)
+	app.InfoLog.Println("Activation emailUrl Generated")
+
 	//create email message
 	msg := Message{
 		To:       u.Email,
@@ -106,12 +117,14 @@ func (app *Config) PostRegisterPage(w http.ResponseWriter, r *http.Request) {
 	}
 	app.sendemail(msg)
 	app.Session.Put(r.Context(), "flash", "Confirmation Mail Sent. Check your email to verify your mail.")
+	app.InfoLog.Println("Activation Email Sent to Mail ")
 	http.Redirect(w, r, "/login", http.StatusSeeOther)
 
 }
 
 // send activation email to confirm we have the valid email address
 func (app *Config) ActivateAccount(w http.ResponseWriter, r *http.Request) {
+	app.InfoLog.Println("Activate Email Page gottten")
 	// validate url
 	url := r.RequestURI
 	testUrl := fmt.Sprintf("http://localhost%s", url)
@@ -138,43 +151,127 @@ func (app *Config) ActivateAccount(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/", http.StatusSeeOther)
 		return
 	}
-	app.Session.Put(r.Context(), "flash", "Account activated . Ypu can login.")
+	app.Session.Put(r.Context(), "flash", "Account activated. You can login.")
 	http.Redirect(w, r, "/login", http.StatusSeeOther)
-
-	// generate an invoice
-
-	// send an email with attachments
-
-	// send an email with the invoice attached
-
-	//subscribe the User is an Account
+	log.Println("Account Activated")
 }
+
 func (app *Config) SubcribeToPlan(w http.ResponseWriter, r *http.Request) {
 	// get the id of the plan that is chosen
+	id := r.URL.Query().Get("id")
 
+	planID, err := strconv.Atoi(id)
+	if err != nil {
+		app.ErrorLog.Println("Error Getting PlanID:", err)
+		return
+	}
 	// get the plan from the database
-
+	plan, err := app.Models.Plan.GetOne(planID)
+	if err != nil {
+		app.Session.Put(r.Context(), "error", "Unable to find plan")
+		http.Redirect(w, r, "/members/plans", http.StatusSeeOther)
+	}
 	// get the user from the session
+	user, ok := app.Session.Get(r.Context(), "user").(database.User)
+	if !ok {
+		app.Session.Put(r.Context(), "error", "Log in first")
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		return
+	}
+	// generate an invoice ane email in it
+	app.Wait.Add(1)
+	go func() {
+		defer app.Wait.Done() //decrement waitgroup
 
-	// generate an invoice
-
-	// send an email with the invoice attached
-
+		invoice, err := app.getInvoice(user, plan)
+		if err != nil {
+			app.ErrorChan <- err
+		}
+		//send an email
+		msg := Message{
+			To:       user.Email,
+			Subject:  "Your Invoice",
+			Data:     invoice,
+			Template: "invoice",
+		}
+		app.sendemail(msg)
+	}()
 	// generate a manual
-
-	// send an email with the manual attached
-
+	app.Wait.Add(1)
+	go func() {
+		defer app.Wait.Done()
+		pdf := app.generateManual(user, plan)
+		//write to a file
+		err := pdf.OutputFileAndClose(fmt.Sprintf("./tmp/%d_manual.pdf", user.ID))
+		if err != nil {
+			app.ErrorChan <- err
+			return
+		}
+		// send an email with the manual attached
+		msg := Message{
+			To:      user.Email,
+			Subject: "Your Manual",
+			Data:    "Your User Manual is attached",
+			AttachmentMap: map[string]string{
+				"Manual.pdf": fmt.Sprintf("./tmp/%d_manual.pdf", user.ID),
+			},
+		}
+		app.sendemail(msg)
+		//test error chan
+		app.ErrorChan <- errors.New("Some Custom Error")
+	}()
 	// subscribe the user to an account
-
+	err = app.Models.Plan.SubscribeUserToPlan(user, *plan)
+	if err != nil {
+		app.Session.Put(r.Context(), "error", "Error Subscribing to Plan")
+		http.Redirect(w, r, "/members/plans", http.StatusSeeOther)
+		return
+	}
+	//update the user in the session from the db
+	u, err := app.Models.User.GetOne(user.ID)
+	if err != nil {
+		app.Session.Put(r.Context(), "error", "Error Getting User From Database")
+		http.Redirect(w, r, "/members/plans", http.StatusSeeOther)
+		return
+	}
+	app.Session.Put(r.Context(), "user", u)
 	// redirect
+	app.Session.Put(r.Context(), "flash", "Subscribed")
+	http.Redirect(w, r, "/members/plans", http.StatusSeeOther)
+}
+
+func (app *Config) getInvoice(u database.User, plan *database.Plan) (string, error) {
+	app.InfoLog.Println("Amount is ", plan.PlanAmountFormatted)
+	return plan.PlanAmountFormatted, nil //returns the price of the plan
+}
+
+func (app *Config) generateManual(u database.User, plan *database.Plan) *gofpdf.Fpdf {
+	pdf := gofpdf.New("p", "mm", "Letter", "")
+	pdf.SetMargins(10, 13, 10)
+
+	importer := gofpdi.NewImporter()
+
+	time.Sleep(5 * time.Second)
+	t := importer.ImportPage(pdf, "./pdf/manual.pdf", 1, "/MediaBox")
+	pdf.AddPage() //we have a page already
+
+	//use the imported template for the page
+	importer.UseImportedTemplate(pdf, t, 0, 0, 215.9, 0)
+	//set x and y coordinates
+	pdf.SetX(75)
+	pdf.SetY(150)
+
+	pdf.SetFont("Arial", "", 12)
+	pdf.MultiCell(0, 4, fmt.Sprintf("%s %s", u.FirstName, u.LastName), "", "C", false)
+	pdf.Ln(5) //performing line breaks
+
+	//a cell that may span multiple linebreaks
+	pdf.MultiCell(0, 4, fmt.Sprintf("%s User Guide", plan.PlanName), "", "C", false)
+
+	return pdf
 }
 
 func (app *Config) ChooseSubscription(w http.ResponseWriter, r *http.Request) {
-	if !app.Session.Exists(r.Context(), "userID") {
-		app.Session.Put(r.Context(), "warning", "Login to access this page")
-		http.Redirect(w, r, "/login", http.StatusTemporaryRedirect)
-		return
-	}
 	plans, err := app.Models.Plan.GetAll()
 	if err != nil {
 		app.ErrorLog.Println(err)
