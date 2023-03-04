0fdc2371a12477c0a4ae6709a57cec74c9aa4dd7
Philip-21 <philipuzomaobiora@gmail.com>
2023-03-04 16:50:41 +0100

    creating a static dir path

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -10,6 +10,7 @@ import (
 func (app *Config) routes() http.Handler {
 	//create a router
 	mux := chi.NewRouter()
+	// // Define the file server handler for static files.
 
 	//middleware
 	mux.Use(middleware.Recoverer)
@@ -25,31 +26,35 @@ func (app *Config) routes() http.Handler {
 	mux.Post("/register", app.PostRegisterPage)
 	mux.Get("/activate", app.ActivateAccount)
 
+	//gets the static files folder which contains the image
+	fileServer := http.FileServer(http.Dir("./cmd/web/templates/static/")) //.gets to the root of the application
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
+
 	//Mount attaches another http.Handler or chi Router as a subrouter along a routing
 	//attaches handlers for only authenticated users
 	mux.Mount("/members", app.authRouter())
 
 	//Sending Email Synchronously (not in use just for reference)
-	mux.Get("/test-mail", func(w http.ResponseWriter, r *http.Request) {
-		m := Mail{
-			Domain:      "localhost",
-			Host:        "localhost",
-			Port:        1025,
-			Encryption:  "none",
-			FromAddress: "philip@company.com",
-			FromName:    "philip",
-			//make an error chan
-			ErrorChan: make(chan error),
-		}
+	// mux.Get("/test-mail", func(w http.ResponseWriter, r *http.Request) {
+	// 	m := Mail{
+	// 		Domain:      "localhost",
+	// 		Host:        "localhost",
+	// 		Port:        1025,
+	// 		Encryption:  "none",
+	// 		FromAddress: "philip@company.com",
+	// 		FromName:    "philip",
+	// 		//make an error chan
+	// 		ErrorChan: make(chan error),
+	// 	}
 
-		msg := Message{
-			To:      "me@here.com",
-			Subject: "testing mail service",
-			Data:    "Hello World",
-		}
-		//sending email
-		m.SendMail(msg, make(chan error))
-	})
+	// 	msg := Message{
+	// 		To:      "me@here.com",
+	// 		Subject: "testing mail service",
+	// 		Data:    "Hello World",
+	// 	}
+	// 	//sending email
+	// 	m.SendMail(msg, make(chan error))
+	// })
 
 	return mux
 }
@@ -58,7 +63,7 @@ func (app *Config) authRouter() http.Handler {
 	mux := chi.NewRouter()
 	mux.Use(app.Auth)
 
-	mux.Get("/plans", app.ChooseSubscription)
+	mux.Get("/plans", app.ChoosePlans)
 	mux.Get("/subscribe", app.SubcribeToPlan)
 	return mux
 
